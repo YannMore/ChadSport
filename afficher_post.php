9b613840cab6808713a5db427970021a767170b9
YannMore <143260450+YannMore@users.noreply.github.com>
2024-05-03 14:25:09 +0200

    css un peu mieux

--- afficher_post.php
+++ afficher_post.php
@@ -102,11 +102,12 @@ $posts = $query->fetchALL(PDO::FETCH_ASSOC);
     
         <div class="img_text">
             <img class="post-picture" src="<?php echo 'images/posts/'.$post['Id_Post'].'.'.$post['image_post'];?>" alt="image du post"/>
-            <h2 class="description_poste">Description du poste : </h2><br>
+            <h2 class="description_poste">Description : </h2><br>
             <p class="post-text"><?php echo $post['contenu']; ?></p>
         </div>
     
     </fieldset>
+    <div class ="espace_comment">
     <?php
     // Récupère les commentaires
     $sqlQuery = "SELECT *  FROM commentaire WHERE Id_Post = :id_post
@@ -117,55 +118,56 @@ $posts = $query->fetchALL(PDO::FETCH_ASSOC);
     $comments = $commentQuery->fetchAll(PDO::FETCH_ASSOC);
     ?>
     <div class="all_comment">
-    <?php
+        <?php
 
-    if (count($comments) === 0) {
-        ?> <h2>*Aucun commentaire pour ce poste*</h2><br> <?php
-    }
-    
-    else{
-        ?> <h2>Commentaires liés à ce poste</h2> <?php
-    }
+        if (count($comments) === 0) {
+            ?> <h2>*Aucun commentaire pour ce poste*</h2><br> <?php
+        }
+        
+        else{
+            ?> <h2>Commentaires</h2> <?php
+        }
 
-    foreach ($comments as $comment) { ?>
-        <div class="comment">
-                  <?php
-                  $sqlQuery = "SELECT image_profil FROM membre WHERE Id_Membre = :id_comment"; //serait pas mal de faire de la mise en cache image ou qlq chose mais dans notre echelle pas prblm performance
-                  $extensionQuery = $mysqlClient->prepare($sqlQuery);
-                  $extensionQuery->bindParam(':id_comment',$comment['Id_Membre']);
-                  $extensionQuery->execute();
-                  $result = $extensionQuery->fetch(PDO::FETCH_ASSOC);
+        foreach ($comments as $comment) { ?>
+            <div class="comment">
+                    <?php
+                    $sqlQuery = "SELECT image_profil FROM membre WHERE Id_Membre = :id_comment"; //serait pas mal de faire de la mise en cache image ou qlq chose mais dans notre echelle pas prblm performance
+                    $extensionQuery = $mysqlClient->prepare($sqlQuery);
+                    $extensionQuery->bindParam(':id_comment',$comment['Id_Membre']);
+                    $extensionQuery->execute();
+                    $result = $extensionQuery->fetch(PDO::FETCH_ASSOC);
 
-                     $profilePic = 'images/profiles/'.$comment['Id_Membre'].'.'.$result["image_profil"];
-                     $defaultPic = 'images/profiles/defaut.png'; // Image par defaut si pas profil
+                        $profilePic = 'images/profiles/'.$comment['Id_Membre'].'.'.$result["image_profil"];
+                        $defaultPic = 'images/profiles/defaut.png'; // Image par defaut si pas profil
 
-                       if (!file_exists($profilePic)) {
-                      $profilePic = $defaultPic;
-                               }?>
-                     <img class="comment-profile" src="<?php echo $profilePic; ?>" alt="photo de profil d'un commentaire"/>
-                      <p class="pseudo_comment"><?php echo getPseudoById($comment['Id_Membre'], $mysqlClient);?></p>
-            
-            <p class="commentaire"><?php echo $comment['contenu_commentaire']; ?></p>
+                        if (!file_exists($profilePic)) {
+                        $profilePic = $defaultPic;
+                                }?>
+                        <img class="comment-profile" src="<?php echo $profilePic; ?>" alt="photo de profil d'un commentaire"/>
+                        <p class="pseudo_comment"><?php echo getPseudoById($comment['Id_Membre'], $mysqlClient);?></p>
+                
+                <p class="commentaire"><?php echo $comment['contenu_commentaire']; ?></p>
+            </div>
+        <?php } 
+        if (count($comments) !== 0) {
+        ?>
+        <button class="show-more" type="button" onclick="window.location.href='<?php echo $url; ?>'">Voir plus de commentaires</button>
+        <br><br>
+        <?php } ?>
+        <form method="POST" enctype="multipart/form-data">
+        <br>
+        <div class="add_comment"><label for="content"><h3>Ajouter un commentaire</h3></label></div><br>
+        <div class="textarea_comment"><textarea id="content" name="content" minlength="1" maxlength="200" cols = "40" rows="6"></textarea></div>
+        <input type="hidden" name="comment" value="True"> 
+        <input type="hidden" name="Id_Post" value="<?php echo $post['Id_Post']; ?>">
+        <div class="submit_comment"><input type="submit" value="Envoyer"></div>
+        </form>
         </div>
-    <?php } 
-    if (count($comments) !== 0) {
-    ?>
-    <button class="show-more" type="button" onclick="window.location.href='<?php echo $url; ?>'">Voir plus de commentaires</button>
-    <br><br>
-    <?php } ?>
-    <form method="POST" enctype="multipart/form-data">
-    <div class="add_comment"><label for="content"><h3>Ajouter un commentaire</h3></label></div><br>
-    <div class="textarea_comment"><textarea id="content" name="content" minlength="1" maxlength="200" cols = "40" rows="6"></textarea></div>
-    <input type="hidden" name="comment" value="True"> 
-    <input type="hidden" name="Id_Post" value="<?php echo $post['Id_Post']; ?>">
-    <div class="submit_comment"><input type="submit" value="Envoyer"></div>
-    </form>
-    </div>
-    <br>
-    <br>
-    <hr>
-    <br>
-    <br>
+        <br>
+        <br>
+        <hr>
+        <br>
+        <br></div>
     
 <?php } ?>
 
